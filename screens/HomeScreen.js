dc40c409c7417cda83617d9d03993402f0155a02
Ashish <73551845+Ashish2468@users.noreply.github.com>
2021-09-12 00:13:48 +0530

    Text Removed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,7 +11,6 @@ export default function Homescreen() {
 	return (
 		<View>
 			<View style={{ backgroundColor: "#313131", height: "100%", Width: "100%" }}>
-				{/* <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt blanditiis neque accusantium facilis sequi quisquam distinctio rerum? Expedita, reprehenderit ex!</Text> */}
 				<View>
 					<ScrollView >
 						{/* <ScrollView style={{marginBottom:250}}> */}
